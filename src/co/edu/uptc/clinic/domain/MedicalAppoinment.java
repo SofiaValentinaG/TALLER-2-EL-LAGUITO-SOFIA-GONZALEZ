0e820f4c36a08a7f4c4638755f428cc544f1e2f0
SofiaValentinaG <sofiavgonzalez.fajardo@gmail.com>
2026-04-29 16:32:15 -0500

    se hizo la clase medical Appoinment y el repositorio de doctor

---
+++ src/co/edu/uptc/clinic/domain/MedicalAppoinment.java
@@ -0,0 +1,71 @@
+package co.edu.uptc.clinic.domain;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class MedicalAppoinment {
+private int idMedicalAppoiment;
+private Date timeAppoinmet;
+private Patient patient;
+private Doctor doctor;
+public MedicalAppoinment(int idMedicalAppoiment, Date timeAppoinmet, Patient patient, Doctor doctor) {
+	super();
+	this.idMedicalAppoiment = idMedicalAppoiment;
+	this.timeAppoinmet = timeAppoinmet;
+	this.patient = patient;
+	this.doctor = doctor;
+}
+public int getIdMedicalAppoiment() {
+	return idMedicalAppoiment;
+}
+public void setIdMedicalAppoiment(int idMedicalAppoiment) {
+	this.idMedicalAppoiment = idMedicalAppoiment;
+}
+public Date getTimeAppoinmet() {
+	return timeAppoinmet;
+}
+public void setTimeAppoinmet(Date timeAppoinmet) {
+	this.timeAppoinmet = timeAppoinmet;
+}
+public Patient getPatient() {
+	return patient;
+}
+public void setPatient(Patient patient) {
+	this.patient = patient;
+}
+public Doctor getDoctor() {
+	return doctor;
+}
+public void setDoctor(Doctor doctor) {
+	this.doctor = doctor;
+}
+@Override
+public int hashCode() {
+	return Objects.hash(idMedicalAppoiment);
+}
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	MedicalAppoinment other = (MedicalAppoinment) obj;
+
+    return idMedicalAppoiment == other.idMedicalAppoiment;
+}
+@Override
+public String toString() {
+    return "\n=== MEDICAL APPOINTMENT ===\n" +
+           "• Appointment ID: " + idMedicalAppoiment + "\n" +
+           "• Date & Time: " + timeAppoinmet + "\n" +
+           "• Patient: " + patient.getFirstName() + " " + patient.getLastName() + "\n" +
+           "• Doctor: " + doctor.getFirstName() + " "+ doctor.getLastName() +"\n" +
+           "============================";
+}
+
+
+
+ 
+}
---
+++ src/co/edu/uptc/clinic/repository/DoctorRepository.java
@@ -0,0 +1,79 @@
+package co.edu.uptc.clinic.repository;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Objects;
+import co.edu.uptc.clinic.domain.Doctor;
+
+public class DoctorRepository {
+	
+	Set<Doctor> doctors;
+
+	public DoctorRepository(Set<Doctor> doctors) {
+		super();
+		this.doctors = new HashSet<>();
+	}
+	
+	public boolean addDoctor(Doctor doctor) {
+		return this.doctors.add(doctor);
+				
+	}
+	public Set <Doctor> findAll(){
+		return this.doctors;
+	}
+	
+	public Doctor findDoctorById(int idDoctor) {
+		for(Doctor i:this.doctors) {
+			if(i.getIdentificationType().equals(idDoctor)) {
+				return i;
+			}
+		}
+		return null;
+		
+	    }
+	 public boolean updateDoctor(Doctor newDoctor) {
+		 
+		 Doctor oldDoctor= this.findDoctorById(newDoctor.getMedicalId());
+		 if(!Objects.isNull(oldDoctor)) {
+			 
+				if(Objects.isNull(newDoctor.getFirstName())) {
+					newDoctor.setFirstName(oldDoctor.getFirstName());
+				}
+				if(Objects.isNull(newDoctor.getLastName())) {
+					newDoctor.setLastName(oldDoctor.getLastName());
+				}
+				
+				if(Objects.isNull(newDoctor.getSpeciality())) {
+					newDoctor.setSpeciality(oldDoctor.getSpeciality());
+				}
+				if(Objects.isNull(newDoctor.getIdentificationType())) {
+					newDoctor.setIdentificationType(oldDoctor.getIdentificationType());
+					
+				}
+				if(Objects.isNull(newDoctor.getYearsOfExperience())) {
+					newDoctor.setYearsOfExperience(oldDoctor.getYearsOfExperience());
+					
+				}
+				
+				
+				this.doctors.add(newDoctor);
+				this.doctors.remove(oldDoctor);
+				
+				return true;
+			 
+		 }
+		 return false;
+		 
+		 
+	 }
+	 
+	 
+	 public boolean delete(int medicalId) {
+		    return this.doctors.removeIf(doctor -> 
+		        doctor.getMedicalId() == medicalId);
+		}
+	}
+	
+
+
+
